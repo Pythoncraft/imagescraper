ca6600d60694d14c9dc0f3dde9c81866a27a3a55
Pythoncraft <73964453+Pythoncraft@users.noreply.github.com>
2020-11-30 23:42:34 +0200

    commit 3
    # 1 added a folder name creation (os.mkdir) by joining current  directory and the file name
    # 2 auto folder name creation from stripped url string
    # 3 added directory change (os.chdir) to save files to new directory

--- scrapingdownload image.py
+++ scrapingdownload image.py
@@ -4,14 +4,19 @@ import urllib.request
 from bs4 import BeautifulSoup
 import os
 
-def make_soup(url):
+def make_soup(url): #add a folder name  to the function
 	thepage = urllib.request.urlopen(url)
 	soupdata = BeautifulSoup(thepage, 'html.parser')
 	return soupdata
 
-image_links = []
-soup = make_soup('https://officeandhome.eu/catalog/office-chairs') # calling the function
-#os.mkdir(os.path.join(get))
+url = 'https://officeandhome.eu/items/Home%20office/Move%20executive%20sit-stand%20desk'
+soup = make_soup(url) # calling the function
+folder_name = url.split('/')[-1]
+try:
+	os.mkdir(os.path.join(os.getcwd(), folder_name)) # creating new directory (os.mkdir) by joining(os.path.join) current directory path(os.getcwd()) with new folder name
+except:
+	pass
+os.chdir(os.path.join(os.getcwd(), folder_name))
 for images in soup.find_all('img'): # filter the html code for img files
 	links = images['src'] # picking all the images with 'src'
 	if links == 'assets/images/logo.png': # skipping this string in loop
@@ -22,4 +27,5 @@ for images in soup.find_all('img'): # filter the html code for img files
 	with open(name_only + '.jpg', 'wb') as f: # open the file in wirte mode 'w', # 'wb' is the 'write binary' mode
 		im = requests.get(links) # sending individual requests to the links, to get the informatio from them
 		f.write(im.content) # write and save content (information) that the link contains  to file
-	
+# 1 add a folder name  to the function
+# 2 add os.mkdir(os.path.join(get)) join the current and new directories to get the corret path
